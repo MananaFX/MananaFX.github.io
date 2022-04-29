2c8c3ae77f614de4e93d1226d48b6d638dde557d
MananaFX <nbspfjh@126.com>
2022-04-29 09:05:02 +0800

    3.8.1 表单无用更新笔记

--- blog-vue-typescript-master/public/ReviewNotesMd/index.js
+++ blog-vue-typescript-master/public/ReviewNotesMd/index.js
@@ -1,6 +1,7 @@
 import note1 from "./2.微机接口指令.md"
 import note2 from "./3.端口地址译码技术.md"
 import note3 from "./4.中断控制技术(8259A).md"
+import note4 from "./5.计数器和定时电路（8254A）.md"
 // md文件批量引入
 
 // 创建md组件同时设置文章其他信息，单独导出使用
@@ -13,6 +14,9 @@ export const components = {
     },
     note3: {
         name: note3,  //与url绑定且与import绑定
+    },
+    note4:{
+        name: note4
     }
 };
 // 注册全部md组件
--- blog-vue-typescript-master/src/views/Timeline.vue
+++ blog-vue-typescript-master/src/views/Timeline.vue
@@ -20,70 +20,70 @@
         </el-card>
       </el-timeline-item>
     </el-timeline>
-    <el-dialog
-        v-model="dialogVisible"
-        title="Tips"
-        width="30%"
-        :before-close="handleClose"
-    >
-      <el-form :model="form" label-width="120px">
-        <el-form-item label="名字">
-          <el-input v-model="form.title" />
-        </el-form-item>
-        <el-form-item label="内容">
-          <el-input v-model="form.content" />
-        </el-form-item>
-        <el-form-item label="时间">
-          <el-col :span="11">
-            <el-date-picker
-                v-model="form.date1"
-                type="date"
-                placeholder="选一个日期吧"
-                style="width: 100%"
-            />
-          </el-col>
-          <el-col :span="2" class="text-center">
-            <span class="text-gray-500">-</span>
-          </el-col>
-          <el-col :span="11">
-            <el-time-picker
-                v-model="form.date2"
-                placeholder="选一个时间吧"
-                style="width: 100%"
-            />
-          </el-col>
-        </el-form-item>
-        <el-form-item label="large尺寸">
-          <el-switch
-              v-model="value"
-              active-color="#13ce66"
-              inactive-color="#ff4949"
-              active-value="100"
-              inactive-value="0"
-          />
-          <br />
-        </el-form-item>
-        <el-form-item label="颜色选择">
-          <el-radio-group v-model="form.color">
-            <el-radio :label="3">Option A</el-radio>
-            <el-radio :label="6">Option B</el-radio>
-            <el-radio :label="9">Option C</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">Create</el-button>
-          <el-button>Cancel</el-button>
-        </el-form-item>
-      </el-form>
-      <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-        >Confirm</el-button
-        >
-      </span>
-      </template>
-    </el-dialog>
+<!--    <el-dialog-->
+<!--        v-model="dialogVisible"-->
+<!--        title="Tips"-->
+<!--        width="30%"-->
+<!--        :before-close="handleClose"-->
+<!--    >-->
+<!--      <el-form :model="form" label-width="120px">-->
+<!--        <el-form-item label="名字">-->
+<!--          <el-input v-model="form.title" />-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="内容">-->
+<!--          <el-input v-model="form.content" />-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="时间">-->
+<!--          <el-col :span="11">-->
+<!--            <el-date-picker-->
+<!--                v-model="form.date1"-->
+<!--                type="date"-->
+<!--                placeholder="选一个日期吧"-->
+<!--                style="width: 100%"-->
+<!--            />-->
+<!--          </el-col>-->
+<!--          <el-col :span="2" class="text-center">-->
+<!--            <span class="text-gray-500">-</span>-->
+<!--          </el-col>-->
+<!--          <el-col :span="11">-->
+<!--            <el-time-picker-->
+<!--                v-model="form.date2"-->
+<!--                placeholder="选一个时间吧"-->
+<!--                style="width: 100%"-->
+<!--            />-->
+<!--          </el-col>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="large尺寸">-->
+<!--          <el-switch-->
+<!--              v-model="value"-->
+<!--              active-color="#13ce66"-->
+<!--              inactive-color="#ff4949"-->
+<!--              active-value="100"-->
+<!--              inactive-value="0"-->
+<!--          />-->
+<!--          <br />-->
+<!--        </el-form-item>-->
+<!--        <el-form-item label="颜色选择">-->
+<!--          <el-radio-group v-model="form.color">-->
+<!--            <el-radio :label="3">Option A</el-radio>-->
+<!--            <el-radio :label="6">Option B</el-radio>-->
+<!--            <el-radio :label="9">Option C</el-radio>-->
+<!--          </el-radio-group>-->
+<!--        </el-form-item>-->
+<!--        <el-form-item>-->
+<!--          <el-button type="primary" @click="onSubmit">Create</el-button>-->
+<!--          <el-button>Cancel</el-button>-->
+<!--        </el-form-item>-->
+<!--      </el-form>-->
+<!--      <template #footer>-->
+<!--      <span class="dialog-footer">-->
+<!--        <el-button @click="dialogVisible = false">Cancel</el-button>-->
+<!--        <el-button type="primary" @click="dialogVisible = false"-->
+<!--        >Confirm</el-button-->
+<!--        >-->
+<!--      </span>-->
+<!--      </template>-->
+<!--    </el-dialog>-->
   </div>
 </template>
 <script lang="ts">
@@ -106,7 +106,7 @@ export default defineComponent({
     LoadingCustom,
   },
   setup() {
-    const dialogVisible = ref(true)
+    const dialogVisible = ref(false)
     const state = reactive({
       isLoadEnd: false,
       isLoading: false,
